5b2122eb314fd5460e723f68d54af45283b09c8b
Maxim <m0536k@gmail.com>
2022-04-23 20:59:25 +0300

    Initial Commit

---
+++ 3DCarView/_DCarViewApp.swift
@@ -0,0 +1,20 @@
+//
+//  _DCarViewApp.swift
+//  3DCarView
+//
+//  Created by apple on 23.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct _DCarViewApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
